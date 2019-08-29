bf27d2fe4a255848955b7274a5a04ff2fdf7163f
Arattel <sizonov2000@gmail.com>
2019-08-29 14:36:56 +0300

    Changed structure to be more readable

---
+++ helpers/feature_extraction.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2 as cv
+
+
+def find_apple_coordinates_pixel_array(pixel_arr, food_color):
+    indices = np.where(np.all(pixel_arr == food_color, axis=-1))
+    indexes = list(zip(indices[1], indices[0]))
+    return np.array(indexes[0])
+
+
+def distances_to_walls(snake_obj, grid_size):
+    up = snake_obj.head[1] - 1
+    left = snake_obj.head[0] - 1
+    down = grid_size[1] - up
+    right = grid_size[0] - left
+    return np.array([up, right, down, left])
+
+
+def feature_vector(snake_object, pixelarr, grid_size, food_color):
+    to_pixels = lambda x: cv.resize(x, grid_size)
+    direction_vector = np.zeros(4)
+    direction_vector[snake_object.direction] = 1
+    pixel_grid = to_pixels(pixelarr)
+    apple_coord = find_apple_coordinates_pixel_array(pixel_grid, food_color)
+    distances = distances_to_walls(snake_object, grid_size)
+    return np.concatenate((direction_vector, apple_coord, distances))
---
+++ helpers/genetic_algorithms.py
@@ -0,0 +1,107 @@
+import torch
+import random
+from helpers.feature_extraction import feature_vector
+import cv2 as cv
+import numpy as np
+from copy import deepcopy
+from more_itertools import sort_together
+
+
+
+class GameNet(torch.nn.Module):
+    def __init__(self):
+        super(GameNet, self).__init__()
+        self.fc1 = torch.nn.Linear(10, 14)
+        self.fc2 = torch.nn.Linear(14, 4)
+        self.act = torch.nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.act(self.fc1(x))
+        x = self.act(self.fc2(x))
+        return x.detach()
+
+    def mutate(self, p=.15):
+        state_dict = self.state_dict()
+        keys = list(state_dict.keys())
+        for key in keys:
+            for index, weight in enumerate(state_dict[key]):
+                if random.uniform(0, 1) <= p:
+                    delta = random.uniform(-.5, .5)
+                    state_dict[key][index] += delta
+        return self.load_state_dict(state_dict)
+
+    def crossover(self, other, p=.3):
+        state_dict0 = deepcopy(self.state_dict())
+        state_dict1 = deepcopy(other.state_dict())
+        keys = list(state_dict1.keys())
+        for key in keys:
+            if random.uniform(0, 1) <= p:
+                x = np.random.randint(1, len(state_dict0[key]))
+                state_dict0[key][:x], state_dict1[key][:x] = state_dict1[key][:x].clone(), state_dict0[key][:x].clone()
+
+        child_net0 = GameNet()
+        child_net1 = GameNet()
+        child_net0.load_state_dict(state_dict0)
+        child_net1.load_state_dict(state_dict1)
+        return child_net0.cuda(), child_net1.cuda()
+
+
+def create_population(individuals=1000):
+    generation = []
+    for i in range(individuals):
+        generation.append(GameNet().cuda())
+    return generation
+
+
+def calculate_fitness(individual):
+    global env
+    observation = env.reset()  # Constructs an instance of the game
+    # Controller
+    game_controller = env.controller
+    # Grid
+    grid_object = game_controller.grid
+    grid_pixels = grid_object.grid
+    # Snake(s)
+    snakes_array = game_controller.snakes
+    snake_object1 = snakes_array[0]
+    to_pixels = lambda x: cv.resize(x, env.grid_size)
+
+    env.render()
+    fitness = 0
+    features = feature_vector(snake_object1, grid_object.grid, env.grid_size, grid_object.FOOD_COLOR)
+    features = torch.cuda.FloatTensor(features)
+    action = np.argmax(individual.forward(features).cpu()).item()
+
+    observation, reward, done, info = env.step(action)
+    fitness += reward
+    while not done:
+        env.render()
+        features = torch.cuda.FloatTensor(
+            feature_vector(snake_object1, observation, env.grid_size, grid_object.FOOD_COLOR))
+        action = np.argmax(individual.forward(features).cpu()).item()
+        observation, reward, done, info = env.step(action)
+        fitness += reward
+    return fitness + 1
+
+
+def create_mating_pool(fitness, population, to_choose):
+    cumulative_fitness = sum(fitness)
+    fitness = list(map(lambda x: x / cumulative_fitness, fitness))
+    fitness, population = sort_together([fitness, population], reverse=True)
+    mating_pool = []
+    indexes = []
+    for i in range(to_choose):
+        p = np.random.uniform(0, 1)
+        for j in range(len(fitness)):
+            if fitness[j] >= p and fitness[j] < p:
+                if j not in indexes:
+                    mating_pool.append(population[j])
+                    indexes.append(j)
+
+        while len(mating_pool) <= i:
+            index_to_append = random.randint(0, len(fitness) - 1)
+            if index_to_append not in indexes:
+                indexes.append(index_to_append)
+                mating_pool.append(population[index_to_append])
+
+    return mating_pool
